a188353603c8426fc5282e5b2f6e36ba161486ab
sathya50 <23124050@srcas.ac.in>
2024-12-07 12:02:42 +0530

    push

---
+++ add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String args[])
+{
+Scanner myobj=new Scanner(System.in);
+System.out.println("enter 1st number:");
+int Firstnumber=myobj.nextInt();
+System.out.println("enter 2nd number:");
+int Secondnumber=myobj.nextInt();
+System.out.println("Firstnumber:"+Firstnumber);
+System.out.println("Secondnumber:"+Secondnumber);
+}
+}
